c383a3472b730ad0634a14adfb1a5c3f1a15ca84
Aaron Kaiser <aaron.kaiser@outlook.de>
2018-06-12 15:43:29 +0200

    Teil der Tabellen hinzugefügt

--- Datenbank.sql
+++ Datenbank.sql
@@ -19,3 +19,55 @@ CREATE TABLE tab_lizenzgeber
 (
 	
 );
+
+CREATE TABLE tab_mitarbeiter
+(
+    MitarbeiterID NUMERIC(5),
+    SozialversicherungsNr NUMERIC(20),
+    Stelle VARCHAR(20),
+    Gehalt NUMERIC(7,2),
+);
+
+CREATE TABLE tab_anschrift
+(
+    Nr NUMERIC(5),
+    PLZ VARCHAR(5),
+    Straße VARCHAR(50),
+    Hausnummer VARCHAR(4)
+);
+
+CREATE TABLE tab_plz_ort
+(
+    PLZ VARCHAR(5),
+    Ort VARCHAR(30)
+);
+
+CREATE TABLE tab_anschrift_von_person
+(
+    SozialversicherungsNr NUMERIC(5),
+    Nr NUMERIC(5)
+);
+
+CREATE TABLE tab_anschrift_von_filiale
+(
+    Nr NUMERIC(5),
+    Name VARCHAR(50)
+);
+
+CREATE TABLE tab_filiale
+(
+    Name VARCHAR(50),
+    Kapazität NUMERIC(5)
+);
+
+CREATE tab_arbeitet_in
+(
+    Name VARCHAR(50),
+    MitarbeiterID NUMERIC(5)
+);
+
+CREATE TABLE tab_hat_exemplar
+(
+    Name VARCHAR(50),
+    ProduktNr NUMERIC(6)
+);
