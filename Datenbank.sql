2a51ac0bc4f9a89ecc29da965ef9010ebc734b19
SebastianRos <sebastian-rosauer@outlook.de>
2018-06-12 15:44:55 +0200

    sql part 2

--- Datenbank.sql
+++ Datenbank.sql
@@ -17,5 +17,40 @@ CREATE TABLE tab_kunde
 
 CREATE TABLE tab_lizenzgeber
 (
-	
+	SozialversicherungsNr NUMERIC(20),
+	Unternehmen VARCHAR(30),
 );
+
+CREATE TABLE tab_leiht
+(
+	ProduktNr NUMERIC(6),
+	KundenNr NUMERIC(5),
+	Datum VARCHAR(14),
+)
+
+CREATE TABLE tab_exemplar
+(
+	Titel VARCHAR(40),
+	Auflage NUMERIC(2),
+)
+
+CREATE TABLE tab_video
+(
+	VideoNr NUMERIC(4),
+	Untenehmen VARCHAR(30),
+	Titel VARCHAR(40),
+	Laufzeit NUMERIC(3),
+	Schauspieler VARCHAR(50),
+	Regisseur VARCHAR(50),
+)
+
+CREATE TABLE tab_hat_genre
+(
+	Bezeichnung VARCHAR(30),
+	VideoNr NUMERIC(4),
+)
+
+CREATE TABLE tab_genre
+(
+	Bezeichnung VARCHAR(30),
+)
